2db68c2568c7cb79fc52805f0840bdaa2d6a5327
Mf4Tn <75338199+Mf4Tn@users.noreply.github.com>
2024-05-21 17:01:57 +0100

    Add files via upload

---
+++ Local IPs Extractor/Local IPs Extractor/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Local_IPs_Extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Extract / Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(360, 362);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 381);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "My Local IP: ";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Discovered IPs";
+            this.columnHeader1.Width = 355;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 381);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(193, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Range IPs Extractor/Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Local IPs Extractor/Local IPs Extractor/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Local_IPs_Extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string local_ip = "";
+        public List<string> ips = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sub_ip = local_ip.Split('.')[0] + '.' + local_ip.Split('.')[1] + '.';
+            int d = 0;
+            for(int i = 0; i < 255; i++)
+            {
+                for(int j = 0; j < 255; j++)
+                {
+                    string ip = sub_ip + i.ToString() + '.' + j.ToString();
+                    ips.Add(ip);
+                    Task.Run(() =>
+                    {
+                        try
+                        {
+                            d++; // threading counter
+                            TaskCompletionSource<bool> res = new TaskCompletionSource<bool>();
+                            if(d > 50)
+                            {
+                                res.SetResult(true);
+                                d = 0;
+                            }
+                            Ping ping = new Ping();
+                            PingReply reply = new Ping().Send(ip);
+                            //MessageBox.Show(ip);
+                            textBox2.Invoke(new MethodInvoker(delegate
+                            {
+                                textBox2.Text = "Checking : "+ip;
+                            }));
+                            if (reply.Status == IPStatus.Success)
+                            {
+                                listView1.Invoke(new MethodInvoker(delegate
+                                {
+                                    //MessageBox.Show("valid ip" + ip);
+                                    listView1.Items.Add(ip);
+                                    
+                                }));
+
+                            }
+                            else
+                            {
+                            }
+                            
+                        }
+                        catch
+                        {
+
+                        }
+                    });
+                    
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress[] addr = ipEntry.AddressList;
+            foreach(IPAddress ip in addr)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) { textBox1.Text = "My Local IP: " + ip.ToString(); local_ip = ip.ToString(); }
+                
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
